65d91ec215c3abf100f52e3092eb7fe210f91c46
Luis Javier Jiménez Fernández <luis77javi@gmail.com>
2019-10-22 20:02:47 +0200

    Implemento el servicio para lista
    Lo que dice el título.

--- src/app/listas/listas.component.ts
+++ src/app/listas/listas.component.ts
@@ -1,10 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
+import { EmpresaService } from '../service/empresa_servico.service';
+import { GlobalQuote } from '../model';
 import { EMPRESAS } from '../listas';
 @Component({
   selector: 'app-listas',
   templateUrl: './listas.component.html',
   styleUrls: ['./listas.component.css']
 })
-export class  ListasComponent {
- empresas = EMPRESAS;
-}
+export class  ListasComponent  implements OnInit{
+
+  datos_json: any;
+  simbolos_empresa: String[] = ['AAPL', 'AMZN', 'GOOG', 'EBAY'];
+  datos_consulta: GlobalQuote[] = [];
+  quoteDataLoaded = true;
+  intervalo_tiempo = timer(1000, 1000);
+  tiempo_refresco = 120;
+
+  constructor(private empresaService: EmpresaService) {}
+  
+  refrescarLista() :void{
+    this.datos_consulta = [];
+    for (const symbol of this.simbolos_empresa){
+      this.busquedaDatos(symbol);
+    }
+  }
+
+  ngOnInit(){
+    this.intervalo_tiempo.subscribe(x => {
+      let tiempo_actual = this.tiempo_refresco;
+      this.tiempo_refresco = tiempo_actual - x % tiempo_actual;
+      if(x%tiempo_actual==0){
+        this.refrescarLista();}
+      });
+    }
+
+    busquedaDatos(symbol){
+      this.empresaService.getEmpresa(symbol).subscribe(data => {
+      const symbolName: string = data['Global Quote']['01. symbol'];
+      const currentPrice: string = data['Global Quote']['05. price'];
+      const change: string = data['Global Quote']['09. change'];
+      const changePercent: string = data['Global Quote']['10. change percent'];
+
+
+      const hasIncreased: boolean = (change.toString().slice(0, 1) === '-') ? false : true;
+      const globalQuote: GlobalQuote = new GlobalQuote(symbolName, currentPrice, change, changePercent, hasIncreased);
+      console.log(globalQuote);
+      this.datos_consulta.push(globalQuote);
+      this.quoteDataLoaded = true;
+      })
+    }
+    
+  }
+
