6123db68463c7df8f81b1e6080b39d091f16cb1e
AlvaroJPM1995 <alvarojesus1995@hotmail.com>
2019-10-22 17:03:58 +0200

    Codigo busqueda empresas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { SearchComponent } from './search/search.component';
+import { ServiceComponent } from './service/service.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+   imports: [
+      BrowserModule,
+      FormsModule
+   ],
+   declarations: [
+      AppComponent,
+      HelloComponent,
+      SearchComponent,
+      ServiceComponent
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
