c25ab7b3f6638476a0b4df4e5aa2d915d96c6cf7
Javi Vivas <javieritovivas@gmail.com>
2019-10-22 17:35:20 +0200

    Añadido petición Http app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -10,7 +11,8 @@ import { ServiceComponent } from './service/service.component';
 @NgModule({
    imports: [
       BrowserModule,
-      FormsModule
+      FormsModule,
+      HttpClientModule
    ],
    declarations: [
       AppComponent,
