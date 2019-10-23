bd878d92192660fbaf0c6ba496383157ae5fbd83
PSYKOTROPIKO <vitilin96@gmail.com>
2019-10-23 17:24:38 +0200

    Creacion Grafica part 1
    lo que dice el titulo.

--- src/app/listas/listas.component.ts
+++ src/app/listas/listas.component.ts
@@ -3,6 +3,7 @@ import { timer } from 'rxjs';
 import { EmpresaService } from '../service/empresa_servico.service';
 import { GlobalQuote } from '../model/global-quote';
 import { EMPRESAS } from '../listas';
+
 @Component({
   selector: 'app-listas',
   templateUrl: './listas.component.html',
